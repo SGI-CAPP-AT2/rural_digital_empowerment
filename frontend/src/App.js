a4e51d21464b003a5cc5743d06e99bff6c8d2331
Shubham Ingale <shubhamingale779@gmail.com>
2025-04-17 18:09:41 +0530

    Init Repo

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Auth from "./pages/Auth";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SGI-CAPP-AT2/prabal-server
+
+go 1.23.3
